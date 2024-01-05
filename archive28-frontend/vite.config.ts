4b55dac42f1dc2ee379d09d6df965c9ea286ab5d
Ciaran Evans <9111975+ciaranevans@users.noreply.github.com>
2024-01-05 15:49:58 +0000

    try vite change for railway

--- archive28-frontend/vite.config.ts
+++ archive28-frontend/vite.config.ts
@@ -5,5 +5,9 @@ export default defineConfig({
 	plugins: [sveltekit()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
+	},
+	server: {
+		port: process.env.PORT || 3000,
+		host: process.env.HOST || '0.0.0.0'
 	}
 });
