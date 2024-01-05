6d904c458352dba582efc073f32ffadecb9e9967
Ciaran Evans <9111975+ciaranevans@users.noreply.github.com>
2024-01-05 15:24:56 +0000

    auth example

--- archive28-frontend/src/routes/+page.svelte
+++ archive28-frontend/src/routes/+page.svelte
@@ -1,7 +1,4 @@
-<script>
-	import { imperialCalendar } from '$lib/imperialCalendar.js';
-	let imperialDate = imperialCalendar();
-	console.log(imperialDate);
+<!-- <script>
 </script>
 
 <div>
@@ -27,3 +24,31 @@
 		font-size: 3vw;
 	}
 </style>
+ -->
+
+ <script>
+	import { signIn, signOut } from "@auth/sveltekit/client"
+	import { page } from "$app/stores"
+	console.log($page.data.session)
+  </script>
+  
+  <h1>SvelteKit Auth Example</h1>
+  <p>
+	{#if $page.data.session}
+	  {#if $page.data.session.user?.image}
+		<span
+		  style="background-image: url('{$page.data.session.user.image}')"
+		  class="avatar"
+		/>
+	  {/if}
+	  <span class="signedInText">
+		<small>Signed in as</small><br />
+		<strong>{$page.data.session.user?.name ?? "User"}</strong>
+		<img src={$page.data.session.user?.image} alt="blah">
+	  </span>
+	  <button on:click={() => signOut()} class="button">Sign out</button>
+	{:else}
+	  <span class="notSignedInText">You are not signed in</span>
+	  <button on:click={() => signIn("github")}>Sign In with GitHub</button>
+	{/if}
+  </p>
