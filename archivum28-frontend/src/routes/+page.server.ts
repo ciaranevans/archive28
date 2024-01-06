0839a712f6321624b9112f6fd397b7b93e8a4f2c
Ciaran Evans <9111975+ciaranevans@users.noreply.github.com>
2024-01-06 15:50:51 +0000

    pin port

--- archivum28-frontend/src/routes/+page.server.ts
+++ archivum28-frontend/src/routes/+page.server.ts
@@ -1,6 +1,6 @@
 /** @type {import('./$types').PageServerLoad} */
 export async function load() {
 	return {
-		resp: await(((await fetch("http://backend.railway.internal")).json()))
+		resp: await(((await fetch("http://backend.railway.internal:6642")).json()))
 	};
 }
