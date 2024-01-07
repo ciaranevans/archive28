931d72ebd07630c1ad21ec8268b7dc2e79fd266c
Ciaran Evans <9111975+ciaranevans@users.noreply.github.com>
2024-01-07 07:44:47 +0000

    Use env var for backend

--- archivum28-frontend/src/routes/+page.server.ts
+++ archivum28-frontend/src/routes/+page.server.ts
@@ -1,6 +1,8 @@
+import { BACKEND_URL } from "$env/static/private"
+
 /** @type {import('./$types').PageServerLoad} */
 export async function load() {
 	return {
-		resp: await(((await fetch("http://backend.railway.internal:6642")).json()))
+		resp: await(((await fetch(BACKEND_URL)).json()))
 	};
 }
